d48721a6a5c60a5de7737895ccfd8a3fd85e8c99
andreslruiz <ing.andreslruiz@gmail.com>
2021-12-30 08:36:16 -0500

    condicionales.py

---
+++ condicionales.py
@@ -0,0 +1,14 @@
+# edad=int(input("Escribe tu edad: "))
+
+# if edad>=18:
+#     print("Es mayor de 18 el man")
+# else:
+#     print("Paila es un pollo")
+
+numero=int(input("Escribe un numero: "))
+if numero>5:
+    print('Es mayor a 5:')
+elif numero==5:
+    print('Numero igual a 5')
+else:
+    print('Es menos a 5')
