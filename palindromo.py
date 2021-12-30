0c09bf777341d1f3d119da0eb4fb36c029344e26
andreslruiz <ing.andreslruiz@gmail.com>
2021-12-30 10:26:39 -0500

    palindromo

---
+++ palindromo.py
@@ -0,0 +1,14 @@
+def palindromo(palabra):
+    palabra=palabra.replace(' ','')
+    palabra=palabra.lower()
+    palabra_rev=palabra[::-1]
+    if palabra==palabra_rev:
+        print("Es palindromo")
+    else: print("No es palindromo")
+
+def run():
+    palabra=input("Escribe una palabra:")
+    palindromo(palabra)
+
+if __name__ =='__main__':
+    run()
